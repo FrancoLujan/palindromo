5492e71a76b382caa3e3f340d942d59847cb6503
franco lujan <francolujann1@gmail.com>
2024-02-12 11:37:13 -0300

    completado

---
+++ logica.js
@@ -0,0 +1,26 @@
+const palabra =   document.querySelector(".palabra");
+const verificar = document.querySelector(".boton");
+const comprobacion = document.querySelector(".valides");
+
+
+
+setInterval(()=> {verificar.onclick = ()=>{
+    
+    let text = palabra.value
+    if(text != ""){
+        let reversa = [... text].reverse().join("")
+        if(text == reversa){
+            comprobacion.style.color = "rgb(52, 255, 1)"
+            comprobacion.style.textShadow = "0px 0px 0.2px white"
+            comprobacion.textContent = "PALINDROMO"
+        }
+        else{
+            comprobacion.style.textShadow = "0px 0px 0.2px black"
+            comprobacion.textContent = "NO PALINDROMO"
+            comprobacion.style.color = "rgb(225, 6 , 5)"
+
+        }
+    }else{
+        
+    }
+}}, 10)
